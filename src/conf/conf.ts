c6161bed2f64e921e6e4f58ee894df2e2df96b0f
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2024-05-15 14:48:35 +0600

    feat: init localization

--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -1,5 +1,6 @@
 const conf = {
-  url: String(import.meta.env.VITE_APP_API_URL),
+    url: String(import.meta.env.VITE_APP_API_URL),
+    app_env: String(import.meta.env.VITE_APP_ENV),
 };
 
 export default conf;
