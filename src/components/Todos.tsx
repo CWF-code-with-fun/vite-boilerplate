8058b03e41b779700781bb037b42c43982875071
Jubayer Al Alam <jubayer.alam@bjitgroup.com>
2024-05-17 10:47:45 +0600

    VS code auto format configuration added.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  addTodo,
-  updateTodo,
-  toggleTodo,
-  removeTodo,
-} from "./features/todo/todoSlice";
+import { addTodo, updateTodo, toggleTodo, removeTodo } from "./features/todo/todoSlice";
 
 const Todos = () => {
-  const todos = useSelector((state: any) => state.todos);
-  console.log("todos", todos);
-  return <div>Todos</div>;
+    const todos = useSelector((state: any) => state.todos);
+    console.log("todos__ss", todos);
+    return <div>Todos</div>;
 };
 
 export default Todos;
