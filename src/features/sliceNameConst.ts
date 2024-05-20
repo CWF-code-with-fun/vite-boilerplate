1cb62a6881735a9fc167ae0549dcb4b45986cacc
Zulkar Nayin <zulkar.nayin@bjitgroup.com>
2024-05-20 15:48:39 +0600

    feat: add react helmet

--- src/features/sliceNameConst.ts
+++ src/features/sliceNameConst.ts
@@ -3,5 +3,4 @@ export const SLICE_NAME = {
     REDUCER_PATH: "api",
     TO_DO: "todos",
     USER: "user",
-    TO_DO: "todos",
 };
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 
 import { RootPath } from "./routes";
 import AddTodo from "@/components/AddTodo/AddTodo";
@@ -9,6 +10,11 @@ function App() {
     const { t } = useTranslation();
     return (
         <div className="container">
+            <Helmet>
+                <title>sample Title</title>
+                <meta name="description" content="sample component" />
+            </Helmet>
+
             <AddTodo>Add Todo</AddTodo>
             <Todos />
             <div>{t("learn")}</div>
