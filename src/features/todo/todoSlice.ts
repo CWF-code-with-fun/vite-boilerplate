1c448689618b5a8c10b5bdfd9167538852a4a54b
Imamul Hassan <imamul.hassan@bjitgroup.com>
2024-05-20 12:44:53 +0600

    UI library configured.
    refactor: Update file paths and imports for AddTodo component

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,31 +1,33 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
+import { Todo, TodoLibrary } from "./types";
+import { SLICE_NAME } from "../sliceNameConst";
 
-const initialState = {
-    todos: [{ completed: false, id: String(1), text: "Learn React" }],
+const initialState: TodoLibrary = {
+    all: [{ completed: false, id: String(1), text: "Learn React" }],
 };
 
 export const todoSlice = createSlice({
     initialState,
-    name: "todos",
+    name: SLICE_NAME.TO_DO,
     reducers: {
-        addTodo: (state, action) => {
-            state.todos.push({
+        addTodo: (state, action: PayloadAction<Todo["text"]>) => {
+            state.all.push({
                 completed: false,
                 id: nanoid(),
                 text: action.payload,
             });
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter(todo => todo.id !== action.payload);
+            state.all = state.all.filter(todo => todo.id !== action.payload);
         },
         toggleTodo: (state, action) => {
-            const todo = state.todos.find(todo => todo.id === action.payload);
+            const todo = state.all.find(todo => todo.id === action.payload);
             if (todo) {
                 todo.completed = !todo.completed;
             }
         },
         updateTodo: (state, action) => {
-            const todo = state.todos.find(todo => todo.id === action.payload.id);
+            const todo = state.all.find(todo => todo.id === action.payload.id);
             if (todo) {
                 todo.text = action.payload.text;
             }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,15 @@
 import { useTranslation } from "react-i18next";
 
-import AddTodo from "../components/AddTodo/AddTodo";
-import LanguageToggle from "../components/LanguageToggle";
-import Todos from "../components/Todos";
-
 import { RootPath } from "./routes";
+import AddTodo from "@/components/AddTodo/AddTodo";
+import Todos from "@/components/Todos";
+import LanguageToggle from "@/components/LanguageToggle";
 
 function App() {
     const { t } = useTranslation();
     return (
         <div className="container">
-            <AddTodo>children</AddTodo>
+            <AddTodo>Add Todo</AddTodo>
             <Todos />
             <div>{t("learn")}</div>
             <LanguageToggle />
